187a67af1e8dd4714cfd53c797fa3560653d754f
hiroshi tkn <wellz-tkn@hotmail.com>
2021-01-09 23:37:32 -0200

    updating map

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -8,7 +8,7 @@ export function fetchProducts() {
 }
 
 export function fetchLocalMapBox(local: string) {
-    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapBoxToken}`)
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapBoxToken}`);
 }
 
 export function saveOrder(payLoad: OrderPayLoad){
